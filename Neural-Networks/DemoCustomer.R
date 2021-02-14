58db9ae358e4c0b929f30bb201d3d43904faf7ad
Soham Salkar <salkarsoham2@gmail.com>
2021-02-14 17:40:47 +0530

    Customer csv added

---
+++ Neural-Networks/DemoCustomer.R
@@ -0,0 +1,6 @@
+Product <- read.table("/Users/sohamsalkar/R-Programming/Neural-Networks/Data Files/Product.txt",header = TRUE, sep ="\t" )
+str(Product)
+
+Customer <- read.csv("/Users/sohamsalkar/R-Programming/Neural-Networks/Data Files/Customer.csv", header = TRUE)
+Customer
+View(Customer)
